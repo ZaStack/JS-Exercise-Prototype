768a60c0f131e9d5fb022d9fa8f7fa469d88986b
Zach Stack <stackz.personal@gmail.com>
2019-12-18 13:32:09 -0800

    Complete task 2

--- index.js
+++ index.js
@@ -54,16 +54,13 @@ function Person(name, age) {
 
   Person.prototype.poop = function (){
     this.stomach.length = 0;
-  };
+  }
 
   Person.prototype.toString = function(){
     return `${this.name}, ${this.age}`;
   }
 
 
-const neo = new Person({
-  name: 'Neo'
-})
 
 /*
   TASK 2
@@ -79,9 +76,23 @@ const neo = new Person({
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons){
+    this.tank = this.tank + gallons;
+  
+}
+
+Car.prototype.drive = function(distance, milesPerGallon){
+  this.odometer = this.odometer + distance; 
+  this.tank = this.tank - milesPerGallon;
+}
+
 
 /*
   TASK 3
